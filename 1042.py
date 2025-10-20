88e06d3397c6869e977c27c93425fe2d589837c2
maimoonaprity <89506567+maimoonaprity@users.noreply.github.com>
2025-10-20 11:30:29 +0600

    updated

---
+++ 1042.py
@@ -0,0 +1,16 @@
+n= map(int, input().split())
+number_list= list(n)
+
+number_list2=number_list.copy()
+
+number_list2.sort()
+
+for item in number_list2:
+    print(item)
+print('')   
+for item in number_list:
+    print(item)    
+
+
+
+    

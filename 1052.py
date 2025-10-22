9c6d8e0175a42702aed1664f376691dd9f5132e9
maimoonaprity <89506567+maimoonaprity@users.noreply.github.com>
2025-10-22 08:53:45 +0600

    uploaded

---
+++ 1052.py
@@ -0,0 +1,21 @@
+
+def identify_month(month,a):
+    print(month.get(a))
+
+
+month={
+    1:"January",
+    2:"February",
+    3:"March",
+    4:"April",
+    5:"May",
+    6:"June",
+    7:"July",
+    8:"August",
+    9:"September",
+    10:"October",
+    11:"November",
+    12:"December"
+}
+a= int(input())
+identify_month(month,a)

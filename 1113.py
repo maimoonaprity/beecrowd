fbe954cc4c9cad4d9380ff7de5b6255c90e9557b
maimoonaprity <maimoonaprity@gmailcom>
2025-10-27 09:28:05 +0600

    uploaded

---
+++ 1113.py
@@ -0,0 +1,14 @@
+while True:
+    a , b = map(int,input().split())
+    
+    if a == b:
+       break
+
+    elif a < b:
+        print("Crescente")
+
+    else:
+        print("Decrescente") 
+   
+  
+

4fce25773a828efccff7ab0ea952694f1505d18d
maimoonaprity <maimoonaprity@gmailcom>
2025-10-28 13:27:38 +0600

    upload

---
+++ 1117.py
@@ -0,0 +1,21 @@
+
+valid =[]
+
+while len(valid)<2:
+
+   i = float(input())
+   if 0<= i <=10:
+        valid.append(i) 
+   else:
+      print("nota invalida")  
+print(f"media = {(valid[0]+valid[1])/2:.2f}")        
+
+         
+
+
+
+
+     
+
+        
+
